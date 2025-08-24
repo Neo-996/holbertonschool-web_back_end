14691a3880b8899366064fd8d2f3fe260a3be7aa
root <abdulelahmalshehri@gmail.com>
2025-08-24 20:00:25 +0000

    11

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,11 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(); // Returns an instance of the same class with no arguments
+  }
+}
